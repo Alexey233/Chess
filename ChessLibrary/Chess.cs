0b212186feda91c78a208adbb2b155362ae04c50
Алексей <@DESKTOP-UK3OU30>
2022-02-23 13:54:12 +0300

    Начальное оформление библиотеки ChessLibrary

---
+++ ChessLibrary/Chess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChessLibrary
+{
+    public class Chess
+    {
+        string fen;
+        
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.fen = fen;
+        }
+
+        public Chess Move(string move)
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess;
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
+    }
+}
